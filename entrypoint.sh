a4b8eaf67405a3caebdc37cfb9385eb5a0bfdf85
Owen Barton <owen.barton@civicactions.com>
2017-12-29 15:14:43 -0800

    Add $ONESHOT environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,26 @@
 # ($SLEEP environment variable) and a notification command 
 # ($NOTIFY_COMMAND environment variable), for example a curl webhook
 # notification (e.g. to Slack).
-if [ -z "${SLEEP}" ]; then
-  # Default to every 6 hours
-  SLEEP=21600
+# Alternatively if $ONESHOT is set then the command and notify will be run
+# once only.
+run() {
+	/opt/google-cloud-auto-snapshot.sh
+	if [ -n "${NOTIFY_COMMAND}" ]; then
+		echo "Running notify command: $NOTIFY_COMMAND"
+		bash -c "$NOTIFY_COMMAND"
+	fi
+}
+
+if [ -z "${ONESHOT}" ]; then
+	run
+else
+	if [ -z "${SLEEP}" ]; then
+		# Default to every 6 hours
+		SLEEP=21600
+	fi
+	while true; do
+		run
+		echo "Sleeping for $SLEEP seconds"
+		sleep $SLEEP
+	done
 fi
-while true; do
-  /opt/google-cloud-auto-snapshot.sh
-  if [ -n "${NOTIFY_COMMAND}" ]; then
-    echo "Running notify command: $NOTIFY_COMMAND"
-    bash -c "$NOTIFY_COMMAND"
-  fi
-  echo "Sleeping for $SLEEP seconds"
-  sleep $SLEEP
-done
